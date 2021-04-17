185af3c09551715fb50122e408e717461a78d8f3
Abel <hi@abelcastro.dev>
2021-04-17 21:14:46 +0200

    Fix test with modify_settings and call migrations

--- app/example_app/tests/test_models.py
+++ app/example_app/tests/test_models.py
@@ -1,10 +1,15 @@
-from django.test import TestCase
+from django.core.management import call_command
+from django.test import TestCase, modify_settings
 
 from example_app.tests.test_app.models import TestModel
 
 
+@modify_settings(INSTALLED_APPS={
+    'append': 'example_app.tests.test_app',
+})
 class TestOverridingInstalledApps(TestCase):
     def setUp(self):
+        call_command('migrate', run_syncdb=True)
         self.test_model = TestModel.objects.create(
             field_a=1,
             field_b=2,
